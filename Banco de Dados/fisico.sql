619b744f4fe2217b6cc562306bcdd6b1d275ab21
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-04 15:31:02 -0300

    atualizando projeto fisico

--- Banco de Dados/fisico.sql
+++ Banco de Dados/fisico.sql
@@ -1,40 +1,123 @@
-create database db_cadastro;
+-- MySQL Workbench Forward Engineering
 
-use db_cadastro;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-create table pessoa(
-   id int auto_increment not null,
-   nome varchar(45) not null,
-   rg varchar(45) not null,
-   cpf char(11) not null,
-   data_nascimento date not null,
-   email varchar(255),
-   telefone varchar(11),
-   endereco varchar(255),
-   primary key (id)
-);
+-- -----------------------------------------------------
+-- Schema db_mvc
+-- -----------------------------------------------------
 
-create table categoria_produto(
-   id int auto_increment not null,
-   descricao varchar(100) not null,
-   primary key(id)
- );
+-- -----------------------------------------------------
+-- Schema db_mvc
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_mvc` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema db_cadastro
+-- -----------------------------------------------------
 
+-- -----------------------------------------------------
+-- Schema db_cadastro
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_cadastro` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `db_mvc` ;
 
-create table produto(
-    id int auto_increment not null,
-    nome varchar(45) not null,
-    preco varchar(100) not null,
-    descricao varchar(100) not null, 
-    primary key(id)
-);
-
-create table funcionario(
-	id int auto_increment not null,
-    nome varchar(45) not null,
-    cargo varchar(45) not null,
-    primary key(id)
-);
+-- -----------------------------------------------------
+-- Table `db_mvc`.`pessoa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_mvc`.`pessoa` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `cpf` CHAR(11) NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `db_mvc`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_mvc`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
 
+USE `db_cadastro` ;
+
+-- -----------------------------------------------------
+-- Table `db_cadastro`.`categoria_produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_cadastro`.`categoria_produto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `db_cadastro`.`funcionario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_cadastro`.`funcionario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `cargo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `db_cadastro`.`pessoa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_cadastro`.`pessoa` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `rg` VARCHAR(45) NOT NULL,
+  `cpf` CHAR(11) NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `telefone` VARCHAR(11) NULL DEFAULT NULL,
+  `endereco` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `db_cadastro`.`produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_cadastro`.`produto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `preco` VARCHAR(100) NOT NULL,
+  `descricao` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `db_cadastro`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_cadastro`.`usuario` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
