8792fd89121bca9fcac0763dec5177212bdda155
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-17 10:41:39 -0300

    atualizando banco

--- Banco de Dados/fisico.sql
+++ Banco de Dados/fisico.sql
@@ -5,11 +5,11 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema db_mvc
+-- Schema db_cadastro
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema db_mvc
+-- Schema db_cadastro
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `db_cadastro` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
