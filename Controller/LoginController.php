5d2d9aa2db5458e4e982da4c7f79d3556b67c448
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-17 10:41:53 -0300

    correção de erro

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -2,30 +2,11 @@
 
 namespace Projeto_MVC\Controller;
 
-/**
- * Definimos aqui que nossa classe precisa incluir uma classe de outro subnamespace
- * do projeto, no caso a classe PessoaModel do subnamespace Model
- */
-
 use Projeto_MVC\Model\LoginModel;
 
-
-/**
- * Classes Controller são responsáveis por processar as requisições do usuário.
- * Isso significa que toda vez que um usuário chama uma rota, um método (função)
- * de uma classe Controller é chamado.
- * O método poderá devolver uma View (fazendo um include), acessar uma Model (para
- * buscar algo no banco de dados), redirecionar o usuário de rota, ou mesmo,
- * chamar outra Controller.
- */
 class LoginController extends Controller
 {
 
-
-    /**
-     * Os métodos index serão usados para devolver uma View.
-     * Para saber mais sobre métodos estáticos, leia: https://www.php.net/manual/pt_BR/language.oop5.static.php
-     */
     public static function index()
     {
         parent::render('Login/FormLogin');
